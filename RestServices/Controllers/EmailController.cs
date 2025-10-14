13af4a9b5ed1e7b6a45a9458bc7eabc43227bc16
KarthiBalasubramani <karthikeyan.selvisubramani@gmail.com>
2025-10-14 15:24:07 +0530

    New Repository Created and New Program Worked.

---
+++ RestServices/Controllers/EmailController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer1;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+        // POST api/<EmailController>
+        [HttpPost]
+        public void Post([FromBody] Messager value)
+        {
+
+            BuiltInEmailServices mailTransfer = new BuiltInEmailServices(
+
+              value.FromAddress, 
+              value.ToAddress,
+              value.Subject,
+              value.Content,
+              value.GmailAppPassword
+               );
+
+             mailTransfer.MessageTransfer();
+
+        }
+
+    } 
+}
